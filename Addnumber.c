561a71bc6a521703b5f4d1aefc9955b0e2d93e00
Md Arman Hasan <123898576+Armgithu@users.noreply.github.com>
2023-09-06 15:16:06 +0530

    Add files via upload

---
+++ Addnumber.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+     int sum;
+    int num1,num2;
+    printf("Enter the first number");
+    scanf("%d",&num1);
+    printf("Enter the second number");
+    scanf("%d",&num2);
+      sum = num1+num2;
+    printf("the sum is: %d\n",sum);
+    return 0;
+}
+    
+
+
+
+
