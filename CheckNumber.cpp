6728bfee4908250b07ff0af32b9e11982ccc5026
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2023-10-05 23:48:20 +0530

    Add files via upload

---
+++ CheckNumber.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter the number");
+    scanf("%d",&number);
+    if(number>0){
+        printf("The number is positive number");
+    }
+    else if (number == 0){
+        
+        printf("The number is Zero");
+
+    }
+        
+        
+        else{
+             printf(" The number is negative number");
+            
+        }
+        return 0;
+        }
+        
